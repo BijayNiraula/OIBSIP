1faadf7d39f15e48458eb8052316c8939f73828b
bijay niraula <bijayniraula7@gmail.com>
2023-09-27 12:51:03 +0545

    fix session bug

--- Online Pizza Ordering Project/Backend/src/server.ts
+++ Online Pizza Ordering Project/Backend/src/server.ts
@@ -16,6 +16,7 @@ import { setupSocketIO } from "./sockets";
 const PORT = process.env.port || 8000;
 
 const app = express();
+app.set("trust proxy", 1);
 
 const server = http.createServer(app)
 setupSocketIO(server)
