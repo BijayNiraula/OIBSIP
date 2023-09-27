c48475e76ad57b9d9be8d78608a9a703f830db1f
bijay niraula <bijayniraula7@gmail.com>
2023-09-27 12:41:36 +0545

    fix session bug

--- Online Pizza Ordering Project/Backend/src/middleware/sessionMiddleware.ts
+++ Online Pizza Ordering Project/Backend/src/middleware/sessionMiddleware.ts
@@ -1,16 +1,16 @@
-const session = require("express-session");
+const session=require("express-session");
 
-const sessionMiddleware = session({
-  secret: process.env.SESSION_KEY,
-  resave: false,
-  saveUninitialized: true,
-  proxy: true,
-  name: 'MyCoolWebAppCookieName12332112',
-  cookie: {
-    secure: false,
-    httpOnly: false,
-    sameSite: 'none'
-  }
-})
+const sessionMiddleware=session({
+    secret:process.env.SESSION_KEY ,
+    resave: false,
+    saveUninitialized: true,
+    proxy: true, 
+    name: 'session3749912qw', 
+    cookie: {
+      secure: false, 
+      httpOnly: false,
+      sameSite: 'none'
+    }
+  })
 
-export default sessionMiddleware;
+  export default sessionMiddleware;
