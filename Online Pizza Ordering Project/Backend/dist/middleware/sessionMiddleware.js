d9a552db9655de4b7c287eebbfdbf6010264f61a
bijay niraula <bijayniraula7@gmail.com>
2023-09-27 12:11:36 +0545

    fix session bug

--- Online Pizza Ordering Project/Backend/dist/middleware/sessionMiddleware.js
+++ Online Pizza Ordering Project/Backend/dist/middleware/sessionMiddleware.js
@@ -4,7 +4,7 @@ const session = require("express-session");
 const sessionMiddleware = session({
     secret: process.env.SESSION_KEY,
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: true,
     cookie: {
         secure: false, // Allow cookies over both HTTP and HTTPS
     },
